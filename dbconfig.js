8b533758574decf37714763125a102f593c30be8
IVAN KWIATKOWSKI <46645772@ort>
2023-05-22 10:41:00 -0300

    frontend hecho?xd

--- dbconfig.js
+++ dbconfig.js
@@ -1,3 +1,4 @@
+
 const config = {
     user:"Pizzas",
     password:"VivaLaMuzza123",
--- index.js
+++ index.js
@@ -5,33 +5,35 @@ import PizzaService from './src/services/pizzas-services.js';
 import cors from "cors";
 
 const app= express();
-const port = 3000;
+const port = 5000;
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
+app.use(express.static('frontend'));
 
-app.get('/', (req,res) => {
+
+app.get('api/pizzas/', (req,res) => {
     let svc = new PizzaService();
     let respuesta = svc.getAll();
     respuesta.then(listaPizzas => {res.send(listaPizzas)})
     // res.send(respuesta);
 })
 
-app.get('/:id', (req,res) => {
+app.get('api/pizzas/:id', (req,res) => {
     let svc = new PizzaService();
     let respuesta = svc.getById(req.params.id);
     respuesta.then(listaPizzas => {res.send(listaPizzas)})
     // res.send(respuesta);
 })
 
-app.delete('/:id', (req,res) => {
+app.delete('api/pizzas/:id', (req,res) => {
     let svc = new PizzaService();
     let respuesta = svc.deleteById(req.params.id);
     respuesta.then(listaPizzas => {res.send(listaPizzas)})
     // res.send(respuesta);
 })
 
-app.put('/:id', async (req,res) => {
+app.put('api/pizzas/:id', async (req,res) => {
     let svc = new PizzaService();
     let pizza = req.body;
     console.log("estoy en el update");
@@ -40,7 +42,7 @@ app.put('/:id', async (req,res) => {
     // res.send(respuesta);
 })
 
-app.post('/', async (req,res) => {
+app.post('api/pizzas/', async (req,res) => {
     let svc = new PizzaService();
     let pizza = req.body;
     console.log(pizza);
